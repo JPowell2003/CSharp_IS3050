897ba8a5d464d8e73c55e03c7986b273ee2cebe9
JPowell2003 <75809385+JPowell2003@users.noreply.github.com>
2024-10-29 12:17:17 -0400

    Add files via upload
    In Class C# Work for 10/29/2024

---
+++ InClass20241029-001/InClass20241029-001/Cooler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass20241029_001
+{
+    /// <summary>
+    /// This class models a cooler that we found in out classroom.
+    /// </summary>
+    internal class Cooler 
+    {
+        //Properties of a cooler
+        private string _color; //prefix private properties with underscore
+        //Add property called cost
+        private double _cost;
+
+        //Declare and instantiate in program.cs
+
+        //Everytime you create a private property add a getter and a setter
+        public string color
+        {
+            get { return _color; }
+            set
+            {
+                if (value.Length > 0)
+                { _color = value; }
+            } //prevents null variables
+        }
+
+        public double cost
+        {
+            get { return _cost; }
+            set
+            {
+                if (value >= 0) //reject negative numbers
+                { _cost = value; }
+            }
+        }
+    }
+}
---
+++ InClass20241029-001/InClass20241029-001/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass20241029_001
+{
+    internal class Program 
+    {
+        static void Main(string[] args) //Entry Point
+        {
+            //declare an object of type cooler
+            Cooler myCooler;
+            myCooler = new Cooler(); //Instantiate that object
+
+            //Need to have a getter and a setter in order to declare properties
+            myCooler.color = "Dirty Gray"; //Invokes the setter
+            Console.WriteLine(myCooler.color); //Invokes the getter
+
+            //Try to put a null value
+            //Has a break point in Cooler at the setter
+            myCooler.color = "";
+            Console.WriteLine("New color of our cooler: " + myCooler.color);
+
+            //Involing the setter and the getter for the cost of cooler
+            myCooler.cost = 15.50;
+            Console.WriteLine("The Price of the Cooler is $" + myCooler.cost);
+            Console.WriteLine("The Price of 20 Coolers is $" + myCooler.cost * 20);
+        }
+    }
+}
